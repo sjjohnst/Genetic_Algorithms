d0521054ea969b6dcfc31f1170672d0fb61d1200
sjjohnst <sjjohnst@uwaterloo.ca>
2021-12-16 16:22:05 -0500

    Reconfigured breeding to be done by controller.py

--- controller.py
+++ controller.py
@@ -1,12 +1,11 @@
 import numpy as np
 from graph import Graph
 from path import Path
-from itertools import combinations
 
 
 class Controller:
 
-    def __init__(self, no_paths, no_vertices, mutation_rate, graph):
+    def __init__(self, no_paths: int, no_vertices: int, mutation_rate: float, graph: Graph):
 
         # Store parameters
         self.no_paths = no_paths
@@ -15,14 +14,21 @@ class Controller:
         self.graph = graph
 
         # Create a set of paths for this graph, sort on their distance
-        self.path_list = [Path(no_vertices, mutation_rate) for i in range(no_paths)]
-        self.path_list.sort(key=lambda x: x.score(self.graph))
-        self.score_list = [path.score(graph) for path in self.path_list]
+        self.path_list = [Path(no_vertices, self.graph) for i in range(no_paths)]
+        self.path_list.sort(key=lambda x: x.score)
+        self.score_list = [path.score for path in self.path_list]
+
+    def get_min_score(self):
+        return min(self.score_list)
+
+    def get_avg_score(self):
+        return np.average(self.score_list)
 
     def draw(self, ax, color='black'):
         """ Draw the graph, and overlay the paths """
 
-        alpha = (1.0 / self.no_paths) # if a particular edge is common in all paths, it will show up as alpha 1.0
+        # If a particular edge is common in all paths, it will show up as alpha 1.0
+        alpha = (1.0 / self.no_paths)
 
         # First, plot all the paths edges
         for path in self.path_list:
@@ -42,28 +48,66 @@ class Controller:
         # Randomly select breeding pairs from the survivors
         # In real world genetics, fitness is also related to reproductive success,
         # but here we only consider survival in 'life'
-        breeding_pairs = np.random.choice(survivors, size=(len(survivors)//2, 2), replace=False)
+        breeding_pairs_1 = np.random.choice(survivors, size=(len(survivors)//2, 2), replace=False)
+        breeding_pairs_2 = np.random.choice(survivors, size=(len(survivors) // 2, 2), replace=False)
         children = []
 
-        # For each of the pairs, create two children and append to chil list
-        for (p1, p2) in breeding_pairs:
-            p3 = p1 + p2
-            p4 = p2 + p1
+        # For each of the pairs, create a child and append to child list
+        for (p1, p2) in breeding_pairs_1:
+            p3 = self.breed(p1, p2)
             children.append(p3)
-            children.append(p4)
+        for (p1, p2) in breeding_pairs_2:
+            p3 = self.breed(p1, p2)
+            children.append(p3)
 
         # If this does not replace all killed paths, breed some more
         diff = self.no_paths - (len(survivors) + len(children))
         while diff > 0:
-            last_pair = np.random.choice(survivors, size=2, replace=False)
-            last_child = last_pair[0] + last_pair[1]
+            [p1, p2] = np.random.choice(survivors, size=2, replace=False)
+            last_child = self.breed(p1, p2)
             children.append(last_child)
             diff = diff - 1
 
         # Finally, concatenate the children with parents
-        self.path_list = np.ndarray.tolist(np.concatenate([survivors, children]))
-        self.path_list.sort(key=lambda x: x.score(self.graph))
-        self.score_list = [path.score(self.graph) for path in self.path_list]
+        self.path_list = list(np.concatenate([survivors, children]))
+        self.path_list.sort(key=lambda x: x.score)
+        self.score_list = [path.score for path in self.path_list]
 
-    def get_min_score(self):
-        return min(self.score_list)
+    def breed(self, p1, p2):
+        """ Breed two Paths """
+
+        # Retrieve a random slice of the path from parent 1 (self)
+        start_ind = np.random.randint(self.no_vertices)
+        slice_length = np.random.randint(low=int(self.no_vertices*0.2), high=int(self.no_vertices*0.8))
+
+        # Retrieve this path section from p1
+        path_section = []
+        j = 0
+        for i in range(slice_length):
+            if start_ind + j >= self.no_vertices:
+                j = -start_ind
+            path_section.append(p1.path[start_ind+j])
+            j = j + 1
+
+        # Now we need to fill in the rest of the path from the mate
+        # Iterate over the mate's path, and add each vertex we encounter not already in new path
+        for vertex in p2.path:
+            if vertex in path_section:
+                pass
+            else:
+                path_section.append(vertex)
+
+        assert (self.no_vertices == len(path_section)), print(len(path_section))
+
+        # Finally, mutate the path using mutation rate probability
+        mutate = np.random.choice([False, True], p=[1-self.mu_rate, self.mu_rate])
+        if mutate:
+            # select two indices randomly
+            indices = np.random.choice(self.no_vertices, 2)
+
+            # Swap the items at selected indices
+            path_section[indices[0]], path_section[indices[1]] = path_section[indices[1]], path_section[indices[0]]
+
+        # Finally, create a new path object and return
+        child = Path(self.no_vertices, self.graph, path=path_section)
+        return child
--- path.py
+++ path.py
@@ -1,16 +1,16 @@
 import numpy as np
 import math
 
+
 class Path:
     """
     A path is a sequence of vertices, representing a walk on a graph.
     Each vertex is seen exactly once, so these paths are loops.
     """
-    def __init__(self, no_vertices, mutation_rate, path=None):
+    def __init__(self, no_vertices, graph, path=None):
 
         # Store parameters
         self.no_vertices = no_vertices
-        self.mutation_rate = mutation_rate
 
         # Define the path
         if path is None:
@@ -27,52 +27,6 @@ class Path:
         shifted_path.append(shifted_path.pop(0))
         self.edges = list(zip(self.path, shifted_path))
 
-    def score(self, graph):
-        """ Calculate the total distance of this path, on graph """
-        score = 0
-
+        self.score = 0
         for (a, b) in self.edges:
-            score += graph.distance(a, b)
-
-        return score
-
-    def __add__(self, mate):
-        """ Breed two Paths """
-
-        # Retrieve a random slice of the path from parent 1 (self)
-        start_ind = np.random.randint(len(self.path))
-        slice_length = np.random.randint(low=int(self.no_vertices*0.2), high=int(self.no_vertices*0.8))
-
-        # Retrieve this path section
-        path_section = []
-        j = 0
-        for i in range(slice_length):
-            if start_ind + j >= len(self.path):
-                j = -start_ind
-            path_section.append(self.path[start_ind+j])
-            j = j + 1
-
-        # Now we need to fill in the rest of the path from the mate
-        # Iterate over the mate's path, and add each vertex we encounter not already in new path
-        for vertex in mate.path:
-            if vertex in path_section:
-                pass
-            else:
-                path_section.append(vertex)
-
-        # Finally, mutate the path using mutation rate probability
-        mutate = np.random.choice([False, True], p=[1-self.mutation_rate, self.mutation_rate])
-        assert(len(self.path) == len(path_section)), print(len(path_section))
-        if mutate:
-            # select two indices randomly
-            indices = np.random.choice(len(self.path), 2)
-
-            # Swap the items at selected indices
-            try:
-                path_section[indices[0]], path_section[indices[1]] = path_section[indices[1]], path_section[indices[0]]
-            except:
-                print(indices)
-
-        # Finally, create a new path object and return
-        child = Path(self.no_vertices, self.mutation_rate, path=path_section)
-        return child
+            self.score += graph.distance(a, b)
